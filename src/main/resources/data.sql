e5d68e5012246a4cdb99ca1f7c4d7f85100ffb35
Martin E. Levensohn <martin.levensohn@gmail.com>
2023-05-23 23:15:01 -0300

    passwords actualizadas en data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,11 @@
 INSERT INTO roles(description, name) VALUES ('Admin role', 'ADMIN');
 INSERT INTO roles(description, name) VALUES ('User role', 'USER');
 
--- passwords => 'admin'
-INSERT INTO app_users(username, password, email) values ('ADMIN', '$2a$10$hjdjJ/M3YF.6h7fIo8PJUOjy34yMt1rF.Y3rhwAt9zJ909vXdCCu.', 'user1@admin.com');
+-- password => 'Admin.1234'
+INSERT INTO app_users(username, password, email) values ('ADMIN', '$2a$10$9vV8AjQUrGwotVl3Y8NllOX.4rP90UOfE750uU6ecBelb2s2733K2', 'user1@admin.com');
 INSERT INTO users_roles(user_id, role_id) values (1, 1);
 INSERT INTO users_roles(user_id, role_id) values (1, 2);
 
-INSERT INTO app_users(username, password, email) values ('user_test', '$2a$10$hjdjJ/M3YF.6h7fIo8PJUOjy34yMt1rF.Y3rhwAt9zJ909vXdCCu.', 'user2@user.com');
+-- password => 'User.1234'
+INSERT INTO app_users(username, password, email) values ('user_test', '$2a$10$P9KRyWdDL3XhYf/Rgb6Cx.DA0W.r.nir4yCvwnbXx6Z6iNBXmdD5a', 'user2@user.com');
 INSERT INTO users_roles(user_id, role_id) values (2, 2);
