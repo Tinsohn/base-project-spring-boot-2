ebf030adf30ec89a18b41eaf071f3ada7e7b7fa6
Martin E. Levensohn <martin.levensohn@gmail.com>
2023-05-26 00:56:21 -0300

    UserRepository extends JpaRepository -> CrudRepository

--- src/main/java/com/mlevensohn/base/repositories/UserRepository.java
+++ src/main/java/com/mlevensohn/base/repositories/UserRepository.java
@@ -1,13 +1,13 @@
 package com.mlevensohn.base.repositories;
 
 import com.mlevensohn.base.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
